cf8bdcdeb2ba54cbb5a2c6aa954362888cf5e1c4
Satyam Mohanty <satyammohantyjee@gmail.com>
2026-07-12 12:14:56 +0530

    TransitOps full stack app

---
+++ frontend/src/api.js
@@ -0,0 +1,24 @@
+const API = "http://localhost:5000/api";
+
+export function getToken() {
+  return localStorage.getItem("token");
+}
+
+export async function request(path, options = {}) {
+  const res = await fetch(`${API}${path}`, {
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: getToken() ? `Bearer ${getToken()}` : "",
+      ...(options.headers || {})
+    }
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.message || "Request failed");
+  }
+
+  return data;
+}
